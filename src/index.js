896dab5cb0aaf13f50fc0791d77796af7ef60fb3
PorfirioEzequiel <islasmartinezporfirioezequiel@gmail.com>
2024-11-09 17:55:23 -0600

    Primer comiit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Form2 from './Form2'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    
+    <Form2/>
+  </React.StrictMode>
+);
+
+
